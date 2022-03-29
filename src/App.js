be61766b22b38a166620feea6a100786e19f7b53
Peter Castle <vagnerpetel2020@gmail.com>
2022-03-29 19:06:33 -0300

    add onEnter, onDelete, onSelectLetter functions.

--- src/App.js
+++ src/App.js
@@ -8,12 +8,57 @@ export const AppContext = createContext();
 
 function App() {
   const [board, setBoard] = useState(boardDefault);
+  const [currAttempt, setCurrAttempt] = useState({ attempt: 0, letterPos: 0 });
+
+  const onSelectLetter = (keyVal, attempt, letterPos) => {
+    if (letterPos > 4) {
+      attempt++;
+      letterPos = 0;
+    }
+    const newBoard = [...board];
+    newBoard[attempt][letterPos] = keyVal;
+    setBoard(newBoard);
+    letterPos++;
+
+    setCurrAttempt({ attempt: attempt, letterPos: letterPos });
+  };
+
+  const onDelete = (attempt, letterPos) => {
+    if (letterPos === 0) return;
+
+    const newBoard = [...board];
+
+    letterPos--;
+
+    newBoard[attempt][letterPos] = "";
+
+    setBoard(newBoard);
+
+    setCurrAttempt({ attempt: attempt, letterPos: letterPos });
+  };
+
+  const onEnter = (attempt, letterPos) => {
+    if (letterPos !== 5) return;
+    setCurrAttempt({ attempt: attempt, letterPos: letterPos });
+    console.log("enter key clicked.");
+  };
+
   return (
     <div className="App">
       <nav>
         <h1>Wordle</h1>
       </nav>
-      <AppContext.Provider value={{ board, setBoard }}>
+      <AppContext.Provider
+        value={{
+          board,
+          setBoard,
+          currAttempt,
+          setCurrAttempt,
+          onSelectLetter,
+          onDelete,
+          onEnter,
+        }}
+      >
         <div className="game">
           <Board />
           <Keyboard />
--- src/components/Key.jsx
+++ src/components/Key.jsx
@@ -1,8 +1,25 @@
-import React from "react";
+import { React, useContext } from "react";
+import { AppContext } from "../App";
 
 function Key({ keyVal, bigKey }) {
+  const { currAttempt, onSelectLetter, onDelete, onEnter } =
+    useContext(AppContext);
+
+  let attempt = currAttempt.attempt;
+  let letterPos = currAttempt.letterPos;
+
+  const selectLetter = () => {
+    if (keyVal === "ENTER") {
+      onEnter(attempt, letterPos);
+    } else if (keyVal === "DELETE") {
+      onDelete(attempt, letterPos);
+    } else {
+      onSelectLetter(keyVal, attempt, letterPos);
+    }
+  };
+
   return (
-    <div id={bigKey ? "big" : ""} className="key">
+    <div id={bigKey ? "big" : ""} className="key" onClick={selectLetter}>
       {keyVal}
     </div>
   );
