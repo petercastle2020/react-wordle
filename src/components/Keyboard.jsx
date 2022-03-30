41730b198514fa7e899ad3df6b47d20afd07c918
Peter Castle <vagnerpetel2020@gmail.com>
2022-03-30 17:25:49 -0300

    add keyboard eventListner.

--- src/components/Keyboard.jsx
+++ src/components/Keyboard.jsx
@@ -1,12 +1,52 @@
-import React from "react";
+import React, { useCallback, useContext, useEffect } from "react";
+import { AppContext } from "../App";
 import Key from "./Key";
 
 function Keyboard() {
   const keys1 = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"];
   const keys2 = ["A", "S", "D", "F", "G", "H", "J", "K", "L"];
   const keys3 = ["Z", "X", "C", "V", "B", "N", "M"];
+
+  const { onSelectLetter, onEnter, onDelete, currAttempt } =
+    useContext(AppContext);
+
+  let attempt = currAttempt.attempt;
+  let letterPos = currAttempt.letterPos;
+
+  const handleKeyboard = useCallback((event) => {
+    if (event.key === "Enter") {
+      onEnter(attempt, letterPos);
+    } else if (event.key === "Backspace") {
+      onDelete(attempt, letterPos);
+    } else {
+      keys1.forEach((key) => {
+        if (event.key.toLowerCase() === key.toLowerCase()) {
+          onSelectLetter(key, attempt, letterPos);
+        }
+      });
+      keys2.forEach((key) => {
+        if (event.key.toLowerCase() === key.toLowerCase()) {
+          onSelectLetter(key, attempt, letterPos);
+        }
+      });
+      keys3.forEach((key) => {
+        if (event.key.toLowerCase() === key.toLowerCase()) {
+          onSelectLetter(key, attempt, letterPos);
+        }
+      });
+    }
+  });
+
+  useEffect(() => {
+    document.addEventListener("keydown", handleKeyboard);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyboard);
+    };
+  }, [handleKeyboard]);
+
   return (
-    <div className="keyboard">
+    <div className="keyboard" onKeyDown={handleKeyboard}>
       <div className="line1">
         {keys1.map((key) => {
           return <Key keyVal={key} />;
