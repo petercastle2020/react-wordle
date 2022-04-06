4e05e44282c02190e93b13d536ffdd18ec8c3d78
Peter Castle <vagnerpetel2020@gmail.com>
2022-04-06 10:43:33 -0300

    add fix for the arrays that would push duplicated items.

--- src/components/Key.jsx
+++ src/components/Key.jsx
@@ -2,8 +2,14 @@ import { React, useContext } from "react";
 import { AppContext } from "../App";
 
 function Key({ keyVal, bigKey, disabled, right, almost }) {
-  const { currAttempt, onSelectLetter, onDelete, onEnter } =
-    useContext(AppContext);
+  const {
+    currAttempt,
+    onSelectLetter,
+    onDelete,
+    onEnter,
+    rightLetters,
+    almostLetters,
+  } = useContext(AppContext);
 
   let attempt = currAttempt.attempt;
   let letterPos = currAttempt.letterPos;
@@ -18,7 +24,27 @@ function Key({ keyVal, bigKey, disabled, right, almost }) {
     }
   };
 
-  // bigKey ? "big" : disabled && "disabled"
+  //checking the index to see if the color have to change from green to yellow;
+
+  // let keyState = "";
+
+  // const rightIndex = rightLetters.includes(keyVal)
+  //   ? rightLetters.indexOf(keyVal)
+  //   : "0";
+  // const almostIndex = almostLetters.includes(keyVal)
+  //   ? almostLetters.indexOf(keyVal)
+  //   : "0";
+
+  // console.log(rightIndex);
+  // console.log(almostIndex);
+
+  // if (rightIndex > almostIndex) {
+  //   keyState = "correct";
+  // } else if (rightIndex < almostIndex) {
+  //   keyState = "almost";
+  // } else {
+  //   console.log("the almost index and right index are the same.");
+  // }
 
   return (
     <div
--- src/components/Letter.jsx
+++ src/components/Letter.jsx
@@ -1,3 +1,4 @@
+import { getQueriesForElement } from "@testing-library/react";
 import React, { useContext, useEffect } from "react";
 import { AppContext } from "../App";
 
@@ -6,10 +7,13 @@ function Letter({ letterPos, attemptVal }) {
     board,
     correctWord,
     currAttempt,
+    rightLetters,
+    almostLetters,
     setDisabledLetters,
     setRightLetters,
     setAlmostLetters,
   } = useContext(AppContext);
+
   const letter = board[attemptVal][letterPos];
 
   const correct = correctWord[letterPos] === letter;
@@ -18,32 +22,69 @@ function Letter({ letterPos, attemptVal }) {
 
   const error = !correct && !almost && letter !== "";
 
-  const letterState =
+  let letterState =
     currAttempt.attempt > attemptVal &&
     (correct ? "correct" : almost ? "almost" : error ? "error" : "");
 
-  /////////////////IN progress, making sure that Key color keep up with the last attempt color/////
+  //////////////////////////////////////////////////////////////////
 
-  const prevLetter = board[attemptVal - 1][letterPos];
+  // I could set a empty arrays with some value, so the first one who gets the letter added, will be the first with that index
 
-  if (
-    letter !== prevLetter && prevLetter === correct
-      ? (letterState = letter)
-      : ""
-  )
-    //////////////////////////////////////////////////////////////////////////////
+  // const rightIndex = rightLetters.includes(letter)
+  //   ? rightLetters.indexOf(letter)
+  //   : "0";
+  // const almostIndex = almostLetters.includes(letter)
+  //   ? almostLetters.indexOf(letter)
+  //   : "0";
 
-    useEffect(() => {
-      if (letter !== "" && !correct && !almost) {
-        setDisabledLetters((prev) => [...prev, letter]);
-      } else if (letter !== "" && correct) {
-        setRightLetters((prev) => [...prev, letter]);
-      } else if (letter !== "" && almost) {
-        setAlmostLetters((prev) => [...prev, letter]);
-      } else {
-        console.log("something went wrong...");
-      }
-    }, [currAttempt.attempt]);
+  // console.log(rightIndex);
+  // console.log(almostIndex);
+
+  // if (rightIndex > almostIndex) {
+  //   letterState = "correct";
+  // } else if (rightIndex < almostIndex) {
+  //   letterState = "almost";
+  // } else {
+  //   console.log("the almost index and right index are the same.");
+  // }
+
+  //////////////////////////////////////////////////////////////
+
+  // var newletter = R > going to the yellow array.
+  // const right = [R,]
+  // const almost = [I,]
+
+  // R = green
+  // I = yellow
+
+  // change the R to "yellow"
+
+  useEffect(() => {
+    if (letter !== "" && !correct && !almost) {
+      setDisabledLetters((prev) => [...prev, letter]);
+    } else if (letter !== "" && correct) {
+      setRightLetters((prev) => {
+        if (prev.includes(letter)) {
+          return [...prev];
+        } else {
+          return [...prev, letter];
+        }
+      });
+    } else if (letter !== "" && almost) {
+      setAlmostLetters((prev) => {
+        if (prev.includes(letter)) {
+          return [...prev];
+        } else {
+          return [...prev, letter];
+        }
+      });
+    } else {
+      console.log("something went wrong...");
+    }
+  }, [currAttempt.attempt]);
+
+  console.log(rightLetters);
+  console.log(almostLetters);
 
   return (
     <div className="letter" id={letterState}>
